b4164893c17f65257b0853c73438c98c596b801d
hasanAkcay <71215285+AkcayHasan@users.noreply.github.com>
2021-02-03 14:21:57 +0300

    bug fixes!

--- app/src/main/java/com/hasanakcay/todoo/model/Weather.kt
+++ app/src/main/java/com/hasanakcay/todoo/model/Weather.kt
@@ -4,6 +4,7 @@ data class Weather(
     val id : Int ?= null,
     val main : String ?= null,
     val description : String ?= null,
-    val icon : String ?= null
+    val icon : String ?= null,
+    val temp : Double ?= null
 ) {
 }
--- app/src/main/java/com/hasanakcay/todoo/service/WeatherAPI.kt
+++ app/src/main/java/com/hasanakcay/todoo/service/WeatherAPI.kt
@@ -1,8 +1,7 @@
 package com.hasanakcay.todoo.service
 
-import com.hasanakcay.todoo.model.Main
 import com.hasanakcay.todoo.model.Weather
-import io.reactivex.Single
+import io.reactivex.Observable
 import retrofit2.http.GET
 
 interface WeatherAPI {
@@ -10,9 +9,6 @@ interface WeatherAPI {
     //api.openweathermap.org/data/2.5/weather?q=Istanbul&appid=1217b60ef51690a245a8c2dcbfd0c959
 
     @GET("/weather?q=Istanbul&appid=1217b60ef51690a245a8c2dcbfd0c959")
-    suspend fun getWeatherData() : Single<Weather>
-
-    @GET("/weather?q=Istanbul&appid=1217b60ef51690a245a8c2dcbfd0c959")
-    suspend fun getTempData() : Single<Main>
+    fun getWeatherData() : Observable<Weather>
 
 }
